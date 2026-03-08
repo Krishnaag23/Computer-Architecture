a71f705e88926efa305e41d47a0aa817c1c64cd9
Krishna Agrawal <krishnaag23@iitk.ac.in>
2026-03-08 16:01:54 +0530

    feat: Instruction Footprint

--- HW1.cpp
+++ HW1.cpp
@@ -11,6 +11,8 @@
 #include "pin.H"
 #include <iostream>
 #include <fstream>
+#include <set>
+#include <map>
 using std::cerr;
 using std::endl;
 using std::string;
@@ -25,13 +27,23 @@ UINT64 threadCount = 0; //total number of threads, including main thread
 UINT64 fastForwardCount = 0; //total number of instructions  in billion, to skip
 UINT64 icount = 0;
 
+UINT64 singleChunkIns = 0;
+UINT64 multipleChunkIns = 0;
+UINT64 singleChunkData = 0;
+UINT64 multipleChunkData = 0;
+
+set<UINT32> instructionAddress;
+set<UINT32> dataAddress;
+map<UINT32, UINT64> instructionSize; 
+map<UINT32, UINT64> dataReadSize;
+
 std::ostream* out = &cerr;
 
 /* ===================================================================== */
 // Command line switches
 /* ===================================================================== */
 KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "", "specify file name for MyPinTool output");
-KNOB< UINT64 > KnobFastForward(KNOB_MODE_WRITEONCE, "pintool", "f", "", "specify fast forward amount in billions");
+KNOB< UINT64 > KnobFastForward(KNOB_MODE_WRITEONCE, "pintool", "f", "0", "specify fast forward amount in billions");
 KNOB< BOOL > KnobCount(KNOB_MODE_WRITEONCE, "pintool", "count", "1",
                        "count instructions, basic blocks and threads in the application");
 
@@ -70,6 +82,35 @@ VOID CountBbl(UINT32 numInstInBbl)
 }
 
 /*!
+ * Add each unique instruction access to a set. 
+ */
+VOID ins_footprint(UINT32 startAddr, UINT32 endAddr){
+	UINT32 nextAddr = startAddr + 32;
+	if (endAddr < nextAddr) singleChunkIns++;
+	else multipleChunkIns++;
+	while(startAddr <= endAddr){
+		instructionAddress.insert(startAddr);
+		startAddr += 32;
+	}
+}
+
+/*!
+ * Add each Data access to a set. 
+ */
+VOID data_footprint(ADDRINT memAddr, UINT32 memSize){
+    UINT32 mAddr = (UINT32) memAddr;
+    // Start address with granularity of 32 bytes.. 
+    UINT32 startAddr = (mAddr/32)*32;
+    UINT32 nextAddr = startAddr + 32;
+    UINT32 endAddr = ((mAddr + memSize)/32)*32;
+    if (endAddr < nextAddr) singleChunkData++;
+    else multipleChunkData++;
+    while(startAddr <= endAddr){
+        dataAddress.insert(startAddr);
+        startAddr += 32;
+    }
+}
+/*!
  * Increase counter for the number of instructions.
  * */
 VOID InsCount(){
@@ -87,13 +128,25 @@ ADDRINT FastForward(void) {
 }
 
 // Analysis routine to exit the application
-void MyExitRoutine(...) {
+void MyExitRoutine(){
 	// Do an exit system call to exit the application.
 	// As we are calling the exit system call PIN would not be able to instrument application end.
 	// Because of this, even if you are instrumenting the application end, the Fini function would not
 	// be called. Thus you should report the statistics here, before doing the exit system call.
 
 	// Results etc
+	*out << "Part C Analysis Results: " << endl;
+	*out << "------------------------------------------------------------" << endl;
+
+	*out << "Instruction Footprint Blocks     			= " << instrAddr.size() << endl;
+    	*out << "Instruction Footprint (in bytes) 			= " << 32*instrAddr.size() << endl;
+    	*out << "Data Footprint Blocks            			= " << dataAddr.size() << endl;
+    	*out << "Data Footprint (in bytes)        			= " << 32*dataAddr.size() << endl;
+	*out << "Number of Single Memory Chunk Instruction access 	=" << singleChunkIns << endl;
+	*out << "Number of Multiple Memory Chunk Instruction access 	=" << multipleChunkIns << endl;
+	*out << "Number of Single Memory Chunk data access 		=" << singleChunkData << endl;
+	*out << "Number of Multiple Memory Chunk data access 		=" << multipleChunkData << endl;
+    	*out << "============================================================" << endl;
 	exit(0);
 }
 
@@ -119,7 +172,28 @@ VOID Trace(TRACE trace, VOID* v)
     for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
     {
         // Insert a call to CountBbl() before every basic bloc, passing the number of instructions
+	BBL_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)Terminate, IARG_END);
+	BBL_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR)MyExitRoutine, IARG_END);
+
         BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)CountBbl, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
+	for(INS ins=BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)){
+            UINT32 startAddr, endAddr;
+            UINT32 insAddr = INS_Address(ins);
+            UINT32 insSize = INS_Size(ins);
+            startAddr = (insAddr/32)*32;
+            endAddr = ((insAddr + insSize)/32)*32;
+            INS_InsertIfCall(ins, IPOINT_BEFORE, (AFUNPTR)FastForward, IARG_END);
+            INS_InsertThenCall(ins, IPOINT_BEFORE, (AFUNPTR)ins_footprint, IARG_UINT32, startAddr, IARG_UINT32, endAddr, IARG_END);
+	}
+	INS_InsertIfCall(ins, IPOINT_BEFORE, FastForward, IARG_END);
+	INS_InsertThenPredicatedCall(ins, IPOINT_BEFORE, MyPredicatedAnalysis, ..., IARG_END); // for instructions with true predicates
+
+	INS_InsertIfCall(ins, IPOINT_BEFORE, FastForward, IARG_END);
+	INS_InsertThenCall(ins, IPOINT_BEFORE, MyAnalysis, ..., IARG_END);  // for all instructions
+
+	INS_InsertCall(ins, IPOINT_BEFORE, InsCount, IARG_END);
+
+
     }
 }
 
@@ -151,17 +225,6 @@ VOID Fini(INT32 code, VOID* v)
     *out << "Number of threads: " << threadCount << endl;
     *out << "===============================================" << endl;
 }
-
-INS_InsertIfCall(ins, IPOINT_BEFORE, Terminate, IARG_END);
-INS_InsertThenCall(ins, IPOINT_BEFORE, MyExitRoutine, ..., IARG_END);
-
-INS_InsertIfCall(ins, IPOINT_BEFORE, FastForward, IARG_END);
-INS_InsertThenPredicatedCall(ins, IPOINT_BEFORE, MyPredicatedAnalysis, ..., IARG_END); // for instructions with true predicates
-
-INS_InsertIfCall(ins, IPOINT_BEFORE, FastForward, IARG_END);
-INS_InsertThenCall(ins, IPOINT_BEFORE, MyAnalysis, ..., IARG_END);  // for all instructions
-
-INS_InsertCall(ins, IPOINT_BEFORE, InsCount, IARG_END);
 
 /*!
  * The main procedure of the tool.
